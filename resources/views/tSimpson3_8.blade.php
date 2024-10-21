2b5eb2745f8731e378e65e9c3c545615eb586141
Carlos Escobar <carloslastre2602@gmail.com>
2024-10-21 17:14:51 -0500

    cambios

--- resources/views/tSimpson3_8.blade.php
+++ resources/views/tSimpson3_8.blade.php
@@ -1,136 +1,57 @@
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Método de Simpson 3/8 - Cálculo de Integrales</title>
-
+    <title>Método de Simpson 3/8</title>
     @vite('resources/css/app.css')
-
 </head>
-<div class="navabr">
-    <nav class="bg-white border-gray-200 dark:bg-gray-900 dark:border-gray-700">
+<div>
+    <nav class="bg-cyan-800 dark:bg-gray-800 fixed top-0 left-0 z-40 w-full">
         <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-          <a href="#" class="flex items-center space-x-3 rtl:space-x-reverse">
-              <img src="https://flowbite.com/docs/images/logo.svg" class="h-8" alt="Flowbite Logo" />
-              <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
-          </a>
-          <button data-collapse-toggle="navbar-dropdown" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-dropdown" aria-expanded="false">
-              <span class="sr-only">Open main menu</span>
-              <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
-                  <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15"/>
-              </svg>
-          </button>
-          <div class="hidden w-full md:block md:w-auto" id="navbar-dropdown">
-            <ul class="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-              <li>
-                <a href="#" class="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500 dark:bg-blue-600 md:dark:bg-transparent" aria-current="page">Home</a>
-              </li>
-              <li>
-                  <button id="dropdownNavbarLink" data-dropdown-toggle="dropdownNavbar" class="flex items-center justify-between w-full py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:w-auto dark:text-white md:dark:hover:text-blue-500 dark:focus:text-white dark:border-gray-700 dark:hover:bg-gray-700 md:dark:hover:bg-transparent">Dropdown <svg class="w-2.5 h-2.5 ms-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
-          <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4"/>
-        </svg></button>
-                  <!-- Dropdown menu -->
-                  <div id="dropdownNavbar" class="z-10 hidden font-normal bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700 dark:divide-gray-600">
-                      <ul class="py-2 text-sm text-gray-700 dark:text-gray-400" aria-labelledby="dropdownLargeButton">
-                        <li>
-                          <a href="#" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Dashboard</a>
-                        </li>
-                        <li>
-                          <a href="#" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Settings</a>
-                        </li>
-                        <li>
-                          <a href="#" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Earnings</a>
-                        </li>
-                      </ul>
-                      <div class="py-1">
-                        <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Sign out</a>
-                      </div>
-                  </div>
-              </li>
-              <li>
-                <a href="#" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Services</a>
-              </li>
-              <li>
-                <a href="#" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Pricing</a>
-              </li>
-              <li>
-                <a href="#" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Contact</a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
-    </div>
-
-    <div>
-     <aside id="default-sidebar" class="fixed top-0 left-0 z-40 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0" aria-label="Sidebar">
-        <div class="h-full px-3 py-4 overflow-y-auto bg-cyan-800 dark:bg-gray-800">
-           <ul class="space-y-2 font-medium">
-              <li>
-                 <a href="#" class="flex items-center p-2 text-white   rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
-                    <svg class="w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 21">
-                       <path d="M16.975 11H10V4.025a1 1 0 0 0-1.066-.998 8.5 8.5 0 1 0 9.039 9.039.999.999 0 0 0-1-1.066h.002Z"/>
-                       <path d="M12.5 0c-.157 0-.311.01-.565.027A1 1 0 0 0 11 1.02V10h8.975a1 1 0 0 0 1-.935c.013-.188.028-.374.028-.565A8.51 8.51 0 0 0 12.5 0Z"/>
+            <div class="flex items-center">
+                <a href="/" class="flex items-center text-white px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-700 rounded-lg">
+                    <svg class="w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 21">
+                        <path d="M16.975 11H10V4.025a1 1 0 0 0-1.066-.998 8.5 8.5 0 1 0 9.039 9.039.999.999 0 0 0-1-1.066h.002Z"/>
+                        <path d="M12.5 0c-.157 0-.311.01-.565.027A1 1 0 0 0 11 1.02V10h8.975a1 1 0 0 0 1-.935c.013-.188.028-.374.028-.565A8.51 8.51 0 0 0 12.5 0Z"/>
                     </svg>
                     <span class="ms-3">Dashboard</span>
-                 </a>
-              </li>
-              <li>
-                 <a href="/trapezoidal" class="flex items-center p-2 text-white  rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
-                    {{-- <svg class="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 18">
-                       <path d="M6.143 0H1.857A1.857 1.857 0 0 0 0 1.857v4.286C0 7.169.831 8 1.857 8h4.286A1.857 1.857 0 0 0 8 6.143V1.857A1.857 1.857 0 0 0 6.143 0Zm10 0h-4.286A1.857 1.857 0 0 0 10 1.857v4.286C10 7.169 10.831 8 11.857 8h4.286A1.857 1.857 0 0 0 18 6.143V1.857A1.857 1.857 0 0 0 16.143 0Zm-10 10H1.857A1.857 1.857 0 0 0 0 11.857v4.286C0 17.169.831 18 1.857 18h4.286A1.857 1.857 0 0 0 8 16.143v-4.286A1.857 1.857 0 0 0 6.143 10Zm10 0h-4.286A1.857 1.857 0 0 0 10 11.857v4.286c0 1.026.831 1.857 1.857 1.857h4.286A1.857 1.857 0 0 0 18 16.143v-4.286A1.857 1.857 0 0 0 16.143 10Z"/>
-                    </svg> --}}
+                </a>
+            </div>
+            <div class="flex space-x-4">
+                <a href="/trapezoidal" class="text-white px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-700 rounded-lg">
                     <span class="flex-1 ms-3 whitespace-nowrap">Trapezoidal</span>
-
-                 </a>
-              </li>
-              <li>
-                 <a href="/jorgeboole" class="flex items-center p-2 text-white  rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
-                    {{-- <svg class="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-                       <path d="m17.418 3.623-.018-.008a6.713 6.713 0 0 0-2.4-.569V2h1a1 1 0 1 0 0-2h-2a1 1 0 0 0-1 1v2H9.89A6.977 6.977 0 0 1 12 8v5h-2V8A5 5 0 1 0 0 8v6a1 1 0 0 0 1 1h8v4a1 1 0 0 0 1 1h2a1 1 0 0 0 1-1v-4h6a1 1 0 0 0 1-1V8a5 5 0 0 0-2.582-4.377ZM6 12H4a1 1 0 0 1 0-2h2a1 1 0 0 1 0 2Z"/>
-                    </svg> --}}
-                    <span class="flex-1 ms-3 whitespace-nowrap"> George Boole</span>
-
-                 </a>
-              </li>
-              <li>
-                 <a href="/Simpson3/8" class="flex items-center p-2 text-white rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
-                    {{-- <svg class="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 18">
-                       <path d="M14 2a3.963 3.963 0 0 0-1.4.267 6.439 6.439 0 0 1-1.331 6.638A4 4 0 1 0 14 2Zm1 9h-1.264A6.957 6.957 0 0 1 15 15v2a2.97 2.97 0 0 1-.184 1H19a1 1 0 0 0 1-1v-1a5.006 5.006 0 0 0-5-5ZM6.5 9a4.5 4.5 0 1 0 0-9 4.5 4.5 0 0 0 0 9ZM8 10H5a5.006 5.006 0 0 0-5 5v2a1 1 0 0 0 1 1h11a1 1 0 0 0 1-1v-2a5.006 5.006 0 0 0-5-5Z"/>
-                    </svg> --}}
-                    <span class="flex-1 ms-3 whitespace-nowrap">Simpson3/8</span>
-                 </a>
-              </li>
-              <li>
-                 <a href="/Simpson1/3" class="flex items-center p-2 text-white  rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
-                    {{-- <svg class="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 20">
-                       <path d="M17 5.923A1 1 0 0 0 16 5h-3V4a4 4 0 1 0-8 0v1H2a1 1 0 0 0-1 .923L.086 17.846A2 2 0 0 0 2.08 20h13.84a2 2 0 0 0 1.994-2.153L17 5.923ZM7 9a1 1 0 0 1-2 0V7h2v2Zm0-5a2 2 0 1 1 4 0v1H7V4Zm6 5a1 1 0 1 1-2 0V7h2v2Z"/>
-                    </svg> --}}
-                    <span class="flex-1 ms-3 whitespace-nowrap">Simpson1/3</span>
-                 </a>
-              </li>
-              <li>
-                 <a href="/simpson-abierto" class="flex items-center p-2 text-white  rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
-                    {{-- <svg class="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 16">
-                       <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 8h11m0 0L8 4m4 4-4 4m4-11h3a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2h-3"/>
-                    </svg> --}}
-                    <span class="flex-1 ms-3 whitespace-nowrap">simpson_abierto</span>
-                 </a>
-              </li>
-
-           </ul>
+                </a>
+                <a href="/jorgeboole" class="text-white px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-700 rounded-lg">
+                    <span class="flex-1 ms-3 whitespace-nowrap">George Boole</span>
+                </a>
+                <a href="/Simpson3/8" class="text-white px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-700 rounded-lg">
+                    <span class="flex-1 ms-3 whitespace-nowrap">Simpson 3/8</span>
+                </a>
+                <a href="/Simpson1/3" class="text-white px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-700 rounded-lg">
+                    <span class="flex-1 ms-3 whitespace-nowrap">Simpson 1/3</span>
+                </a>
+                <a href="/simpson-abierto" class="text-white px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-700 rounded-lg">
+                    <span class="flex-1 ms-3 whitespace-nowrap">Simpson Abierto</span>
+                </a>
+            </div>
         </div>
-     </aside>
-    </div>
+    </nav>
+
+</div>
+
 <body>
-    <div class="p-6 sm:ml-64 bg-gray-50 min-h-screen">
+    <div class="p-6 sm:mix-blend-screen bg-gray-50 min-h-screen flex items-center justify-center">
+        <div class="bg-white w-96 p-6 rounded-lg shadow-md">
         <h1 class="text-3xl font-bold mb-6 text-gray-800">Método de Simpson 3/8 para Cálculo de Integrales</h1>
 
-        <form id="simpsonForm" class="space-y-4 bg-white p-6 rounded-lg shadow-md">
+        <form id="simpsonForm" class="space-y-4">
             <div>
-                <label for="funcion" class="block text-lg font-medium text-gray-700 mb-2">Elige la función \(f(x)\):</label>
-                <select id="funcion" name="funcion" class="block w-full p-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500">
+                <label for="funcion" class="block text-lg font-medium text-gray-700 mb-2">Elige la función
+                    \(f(x)\):</label>
+                <select id="funcion" name="funcion"
+                    class="block w-full p-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500">
                     <option value="x**2">x^2</option>
                     <option value="Math.sin(x)">sin(x)</option>
                     <option value="Math.cos(x)">cos(x)</option>
@@ -140,24 +61,34 @@
             </div>
 
             <div>
-                <label for="customFunction" class="block text-lg font-medium text-gray-700 mb-2">Función personalizada (solo si eliges personalizada):</label>
-                <input type="text" id="customFunction" class="block w-full p-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500" placeholder="Ejemplo: Math.sqrt(x)" disabled />
+                <label for="customFunction" class="block text-lg font-medium text-gray-700 mb-2">Función personalizada
+                    (solo si eliges personalizada):</label>
+                <input type="text" id="customFunction"
+                    class="block w-full p-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500"
+                    placeholder="Ejemplo: Math.sqrt(x)" disabled />
             </div>
 
             <div>
                 <label for="a" class="block text-lg font-medium text-gray-700 mb-2">Límite inferior (a):</label>
-                <input type="number" id="a" class="block w-full p-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500" value="0">
+                <input type="number" id="a"
+                    class="block w-full p-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500"
+                    value="0">
             </div>
 
             <div>
                 <label for="b" class="block text-lg font-medium text-gray-700 mb-2">Límite superior (b):</label>
-                <input type="number" id="b" class="block w-full p-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500" value="1">
+                <input type="number" id="b"
+                    class="block w-full p-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500"
+                    value="1">
             </div>
 
-            <button type="submit" class="w-full bg-blue-500 text-white font-bold py-2 px-4 rounded-md hover:bg-blue-600 transition duration-300">Calcular Integral</button>
+            <button type="submit"
+                class="w-full bg-blue-500 text-white font-bold py-2 px-4 rounded-md hover:bg-blue-600 transition duration-300">Calcular
+                Integral</button>
         </form>
 
-        <h2 class="mt-6 text-2xl font-semibold text-gray-800">Resultado: <span id="resultado" class="text-blue-500"></span></h2>
+        <h2 class="mt-6 text-2xl font-semibold text-gray-800">Resultado: <span id="resultado"
+                class="text-blue-500"></span></h2>
 
         <h3 class="mt-4 text-xl font-medium text-gray-800">Iteraciones:</h3>
         <pre id="iteraciones" class="mt-2 p-4 bg-gray-100 rounded-md"></pre>
@@ -165,7 +96,7 @@
 
 
     <script>
-        document.getElementById('funcion').addEventListener('change', function () {
+        document.getElementById('funcion').addEventListener('change', function() {
             const customFunctionInput = document.getElementById('customFunction');
             if (this.value === 'custom') {
                 customFunctionInput.disabled = false;
@@ -174,7 +105,7 @@
             }
         });
 
-        document.getElementById('simpsonForm').addEventListener('submit', function (e) {
+        document.getElementById('simpsonForm').addEventListener('submit', function(e) {
             e.preventDefault();
 
             // Obtener valores del formulario
@@ -199,7 +130,7 @@
         });
 
         function calcularSimpson38(a, b, funcion) {
-            const delta = (b - a) / 3;  // Usamos (b - a) / 3 para obtener los 4 puntos
+            const delta = (b - a) / 3; // Usamos (b - a) / 3 para obtener los 4 puntos
 
             // Definir una función evaluadora para las funciones matemáticas
             const f = (x) => {
@@ -223,12 +154,15 @@
 
             // Iteraciones detalladas
             const iteraciones = `x1 = ${x1.toFixed(6)}, f(x1) = ${fx1.toFixed(6)}\n` +
-                                `x2 = ${x2.toFixed(6)}, f(x2) = ${fx2.toFixed(6)}\n` +
-                                `x3 = ${x3.toFixed(6)}, f(x3) = ${fx3.toFixed(6)}\n` +
-                                `x4 = ${x4.toFixed(6)}, f(x4) = ${fx4.toFixed(6)}\n` +
-                                `\nIntegral aproximada = ${integral.toFixed(6)}`;
+                `x2 = ${x2.toFixed(6)}, f(x2) = ${fx2.toFixed(6)}\n` +
+                `x3 = ${x3.toFixed(6)}, f(x3) = ${fx3.toFixed(6)}\n` +
+                `x4 = ${x4.toFixed(6)}, f(x4) = ${fx4.toFixed(6)}\n` +
+                `\nIntegral aproximada = ${integral.toFixed(6)}`;
 
-            return { integral, iteraciones };
+            return {
+                integral,
+                iteraciones
+            };
         }
     </script>
 </body>
